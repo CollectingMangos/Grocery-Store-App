148606d39e39c762cae9a3d82e323934dede8618
Ruben <rubenfds@icloud.com>
2025-10-16 08:14:26 +0200

    Refractoring and 1st step to modularisation

--- backend/product_dao.py
+++ backend/product_dao.py
@@ -1,14 +1,30 @@
 import mysql.connector
 
-connection = mysql.connector.connect(user='root', password='', host='127.0.0.1',database='grocery_store')
+def get_all_products():
+    connection = mysql.connector.connect(user='root', password='', host='127.0.0.1',database='grocery_store')
 
-cursor = connection.cursor()
+    cursor = connection.cursor()
 
-query = ('SELECT products.product_id, products.name, products.unit_of_measurement_id, products.price_per_unit, unit_of_measurement.name FROM grocery_store.products inner join grocery_store.unit_of_measurement ON products.unit_of_measurement_id = unit_of_measurement.unit_of_measurement_id')
+    query = ('SELECT products.product_id, products.name, products.unit_of_measurement_id, products.price_per_unit, unit_of_measurement.name FROM grocery_store.products inner join grocery_store.unit_of_measurement ON products.unit_of_measurement_id = unit_of_measurement.unit_of_measurement_id')
 
-cursor.execute(query)
+    cursor.execute(query)
 
-for (product_id,name,unit_of_measurement_id,price_per_unit, name) in cursor:
-    print(product_id,name,unit_of_measurement_id,price_per_unit,name)
+    response = []
 
-connection.close()
+    for (product_id,product_name,unit_of_measurement_id,price_per_unit, name) in cursor:
+        response.append(
+            {
+                "product_id":product_id,
+                "name":product_name,
+                "uom_id":unit_of_measurement_id,
+                "price_per_unit":price_per_unit,
+                "uom_name": name,
+            }
+        )
+
+    connection.close()
+
+    return response
+
+if __name__ == "__main__":
+    print(get_all_products())
