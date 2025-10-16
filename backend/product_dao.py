d34db31ed31c807d7fba30b401d0099b96544bf8
Ruben <rubenfds@icloud.com>
2025-10-16 08:02:30 +0200

    2nd iteration of connection, added uom_name

--- backend/product_dao.py
+++ backend/product_dao.py
@@ -4,11 +4,11 @@ connection = mysql.connector.connect(user='root', password='', host='127.0.0.1',
 
 cursor = connection.cursor()
 
-query = 'SELECT * FROM grocery_store.products'
+query = ('SELECT products.product_id, products.name, products.unit_of_measurement_id, products.price_per_unit, unit_of_measurement.name FROM grocery_store.products inner join grocery_store.unit_of_measurement ON products.unit_of_measurement_id = unit_of_measurement.unit_of_measurement_id')
 
 cursor.execute(query)
 
-for (product_id,name,unit_of_measurement_id,price_per_unit) in cursor:
-    print(product_id,name,unit_of_measurement_id,price_per_unit)
+for (product_id,name,unit_of_measurement_id,price_per_unit, name) in cursor:
+    print(product_id,name,unit_of_measurement_id,price_per_unit,name)
 
 connection.close()
