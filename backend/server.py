17cc46b921d991b9a81dcece41f564c1d70ed651
Ruben <rubenfds@icloud.com>
2025-10-17 15:36:09 +0200

    First end point running successfully

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import product_dao
+from sql_connection import get_sql_connection
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello how are you"
+connection = get_sql_connection()
+
+@app.route("/getProducts", methods=['GET'])
+def get_products():
+    products = product_dao.get_all_products(connection)
+    response = jsonify(products)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 if __name__ == "__main__":
-    print("Starting Python Flask server for Grocery Store App")
+    print("Starting Python Flask server for Grocery Store Management System")
     app.run(port=5000)
     
